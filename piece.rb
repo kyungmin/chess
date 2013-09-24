29144575efe269c1e0433b0ed1a9f72876044c13
Kyungmin Kim and Isaac Murchie <student@appacademy.io>
2013-09-24 13:30:26 -0700

    Add valid_moves method

--- piece.rb
+++ piece.rb
@@ -6,5 +6,17 @@ module Chess
     def initialize(color, position)
       @color, @position = color, position
     end
+
+    def valid_moves
+      moves.select do |pos|
+        valid_move?(pos)
+      end
+    end
+
+    def valid_move?(pos)
+      # duplicate board
+      # make the move
+      # valid if not in check
+    end
   end
 end
