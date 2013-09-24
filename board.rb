b73edf0ce7a1598f588d6eaa27afeb1f34e7b1ef
Kyungmin Kim and Isaac Murchie <student@appacademy.io>
2013-09-24 16:28:37 -0700

    Add move function

--- board.rb
+++ board.rb
@@ -1,29 +1,85 @@
 require "./piece"
+require "./pawn"
+require "./stepping_piece"
+require "./sliding_piece"
 
 module Chess
   class Board
+    attr_reader :grid
+
     def initialize
       @grid = init_grid
     end
 
     def move(pos1, pos2)
+      piece = piece(pos1)
+      raise "Invalid move!" unless piece.valid_move?(pos2)
+
+      @grid[pos1[0]][pos1[1]] = nil
+      @grid[pos2[0]][pos2[1]] = piece
+
     end
 
     def show_grid
-      puts "  0 1 2 3 4 5 6 7"
+      puts "   a  b  c  d  e  f  g  h"
       @grid.each_with_index do |row, index|
-        print "#{index} "
+        print "#{@grid.length-index} "
         row.each do |piece|
-          print piece ? piece : " "
-          print " "
+          print " #{piece ? piece : " "} "
         end
         puts
       end
     end
 
-    def dup
-      raise "Cannot yet duplicate the board"
+    def piece(pos)
+      @grid[pos[0]][pos[1]]
     end
+
+    def pieces(color)
+      pieces = []
+      @grid.each do |row|
+        row.each do |piece|
+          pieces << piece if piece && piece.color == color
+        end
+      end
+
+      pieces
+    end
+
+    def check?(color)
+      king = king(color)
+      pieces(color == :white ?  :black : :white).each do |piece|
+        return true if piece.moves.include?(king.position)
+      end
+
+      false
+    end
+
+    def dup
+      new_board = Board.new
+
+      grid = []
+      # deep duplication of @grid
+      @grid.each do |row|
+        new_row = []
+        row.each do |piece|
+          new_row << (piece ? piece.dup : nil)
+        end
+        grid << new_row
+      end
+
+      new_board.grid = grid
+
+      new_board
+    end
+
+
+    protected
+
+    def grid=(grid)
+      @grid = grid
+    end
+
 
     private
 
@@ -34,20 +90,29 @@ module Chess
 
       {:black => [0, 1], :white => [7, 6]}.each do |color, row|
         8.times do |column|
-          grid[row[1]][column] = Pawn.new(color, [row[1], column])
+          grid[row[1]][column] = Pawn.new(color, [row[1], column], self)
         end
 
-        grid[row[0]][0] = Rook.new(color, [row[0], 0])
-        grid[row[0]][1] = Knight.new(color, [row[0], 1])
-        grid[row[0]][2] = Bishop.new(color, [row[0], 2])
-        grid[row[0]][3] = Queen.new(color, [row[0], 3])
-        grid[row[0]][4] = King.new(color, [row[0], 4])
-        grid[row[0]][5] = Bishop.new(color, [row[0], 5])
-        grid[row[0]][6] = Knight.new(color, [row[0], 6])
-        grid[row[0]][7] = Rook.new(color, [row[0], 7])
+        grid[row[0]][0] = Rook.new(color, [row[0], 0], self)
+        grid[row[0]][1] = Knight.new(color, [row[0], 1], self)
+        grid[row[0]][2] = Bishop.new(color, [row[0], 2], self)
+        grid[row[0]][3] = Queen.new(color, [row[0], 3], self)
+        grid[row[0]][4] = King.new(color, [row[0], 4], self)
+        grid[row[0]][5] = Bishop.new(color, [row[0], 5], self)
+        grid[row[0]][6] = Knight.new(color, [row[0], 6], self)
+        grid[row[0]][7] = Rook.new(color, [row[0], 7], self)
       end
 
       grid
     end
+
+    def king(color)
+      @grid.each do |row|
+        row.each do |piece|
+          return piece if piece.is_a?(King) && piece.color == color
+        end
+      end
+    end
+
   end
 end
