8eba7455a7142955d24db0d7674bca96dd5cf131
Kyungmin Kim and Isaac Murchie <student@appacademy.io>
2013-09-24 17:49:20 -0700

    Fix pawn movement

--- pawn.rb
+++ pawn.rb
@@ -12,16 +12,16 @@ module Chess
       moves_array = []
       new_position = [position[0].send(@direction, 1), position[1]]
 
-      if on_board?(new_position) && !position_occupied_by_us?(new_position)
+      if on_board?(new_position) && !position_occupied?(new_position)
         moves_array << new_position
         next_position = [new_position[0].send(@direction, 1), new_position[1]]
-        if !@moved && !position_occupied_by_other?(new_position) &&
-          (on_board?(next_position) && !position_occupied_by_us?(next_position))
+        if !@moved && !position_occupied?(new_position) &&
+          (on_board?(next_position) && !position_occupied?(next_position))
           moves_array << next_position
         end
       end
 
-      p diagonal_positions = [[position[0].send(@direction, 1), position[1] - 1],
+      diagonal_positions = [[position[0].send(@direction, 1), position[1] - 1],
                           [position[0].send(@direction, 1), position[1] + 1]]
       diagonal_positions.each do |diagonal_position|
         if on_board?(diagonal_position) && position_occupied_by_other?(diagonal_position)
@@ -30,6 +30,11 @@ module Chess
       end
 
       moves_array
+    end
+
+    def position=(pos)
+      @moved = true
+      super(pos)
     end
 
     def to_s
