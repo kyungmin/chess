730803847be264841ebb8fd4407fc1963cc7e688
Kyungmin Kim and Isaac Murchie <student@appacademy.io>
2013-09-25 11:53:55 -0700

    Add alternating players

--- lib/chess.rb
+++ lib/chess.rb
@@ -8,10 +8,23 @@ module Chess
     def play
       board = Board.new
 
-      until board.game_over?
+      player = :white
+      until board.game_over?(player)
         board.show_grid
-        start_pos, end_pos = get_input
-        board.move(start_pos, end_pos)
+
+        while true
+          begin
+            start_pos, end_pos = get_input(player)
+            board.move(start_pos, end_pos, player)
+          rescue InvalidMoveException => e
+            puts e.message
+            next
+          end
+
+          break
+        end
+
+        player = next_player(player)
       end
 
       if board.checkmate?(:white)
@@ -24,14 +37,18 @@ module Chess
 
     private
 
-    def get_input
-      puts "Enter move."
+    def get_input(player)
+      puts "#{player.to_s.capitalize}: Enter move."
       grid_map = {  "h" => 7, "g" => 6, "f" => 5, "e" => 4,
                     "d" => 3, "c" => 2, "b" => 1, "a" => 0,
                     "1" => 7, "2" => 6, "3" => 5, "4" => 4,
                     "5" => 3, "6" => 2, "7" => 1, "8" => 0 }
 
-      input = gets.chomp.gsub(/[^12345678abcdefgh]/, "")
+      # input = gets.chomp.gsub(/[^12345678abcdefgh]/, "")
+      input = gets.chomp
+      if input == "quit"
+        exit
+      end
 
       original = [grid_map[input[1]], grid_map[input[0]]]
       target = [grid_map[input[3]], grid_map[input[2]]]
@@ -39,6 +56,9 @@ module Chess
       [original, target]
     end
 
+    def next_player(player)
+      (player == :white ? :black : :white)
+    end
   end
 end
 
